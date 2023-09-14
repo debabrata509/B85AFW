149137ae10557fdca7a8a7f26822a0bcb8634d4e
Debabrata Thakur <debabratathakur@Debabratas-MacBook-Pro.local>
2023-09-14 08:31:50 +0530

    Initial commit

---
+++ script/test_script1.py
@@ -0,0 +1,19 @@
+from generic.base_setup import Base_SetUp
+
+
+class Test1(Base_SetUp):
+
+    def test1(self):
+        print("executing test")
+        print(self.driver.title)
+
+from generic.base_setup import Base_SetUp
+from generic.excel import Excel
+
+class Test1(Base_SetUp):
+
+    def test1(self):
+        p = Excel.get_cell_data("../test_data/input.xlsx", "Sheet1", 1, 1)
+        print("Data from excel",p)
+        print("executing test")
+        print(self.driver.title)
